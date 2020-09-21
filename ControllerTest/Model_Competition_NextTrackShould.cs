6297416e5cf00672d72e1fbb6e166a591431159a
Nigel Barink <nigelbarink@hotmail.com>
2020-09-21 21:30:01 +0200

    Eerste test toegevoegd en gekeken naar test coverage | Level 3 Taak 1 & 2

---
+++ ControllerTest/Model_Competition_NextTrackShould.cs
@@ -0,0 +1,29 @@
+﻿using Model;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControllerTest
+{
+    [TestFixture]
+    class Model_Competition_NextTrackShould
+    {
+        private static Competition _competition;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _competition = new Competition();
+            
+        }
+        [Test]
+        public void NextTRack_EmptyQueue_ReturnNull()
+        {
+            var result = _competition.NextTrack();
+            Assert.IsNull(result);
+
+        }
+
+    }
+}
