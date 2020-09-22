25b256dde5c7615b83975530afd1a7716e0f2986
Nigel Barink <nigelbarink@hotmail.com>
2020-09-22 10:58:58 +0200

    Toevoegen laatste tests voor de NextTrack() methode. | Level 3 Taak 4

--- ControllerTest/Model_Competition_NextTrackShould.cs
+++ ControllerTest/Model_Competition_NextTrackShould.cs
@@ -35,6 +35,33 @@ namespace ControllerTest
 
             Assert.AreEqual(track, result);
         }
+        [Test]
+        public void NextTrack_OneInQueue_RemoveTrackFromQueue()
+        {
+            var track = new Track("TestTrack", new SectionTypes[0]);
+            _competition.tracks.Enqueue(track);
+            Track result = _competition.NextTrack();
+            result = _competition.NextTrack();
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void NextTrack_TwoInQueue_ReturnNextTrack()
+        {
+            var track1 = new Track("TestTrack1", new SectionTypes[0]);
+            var track2 = new Track("TestTrack2", new SectionTypes[0]);
+
+            _competition.tracks.Enqueue(track1);
+            _competition.tracks.Enqueue(track2);
+
+            Track result = _competition.NextTrack();
+            Assert.AreEqual(track1, result);
+
+            result = _competition.NextTrack();
+
+            Assert.AreEqual(track2, result);
+
+        }
 
     }
 }
