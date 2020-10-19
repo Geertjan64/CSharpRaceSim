e28db274ae37b9566b190c72d314b4977941331e
Nigel Barink <nigelbarink@hotmail.com>
2020-10-19 13:52:15 +0200

    Data Templates | Level 6 Taak 2

---
+++ Model/ParticipantPoints.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    class ParticipantPoints
+    {
+        public string naam { get; set; }
+        public int points { get;  set; }
+    }
+}
---
+++ Model/ParticipantTime.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    class ParticipantTime
+    {
+        public string naam { get; set; }
+        public  TimeSpan time { get; set; }
+    }
+}
