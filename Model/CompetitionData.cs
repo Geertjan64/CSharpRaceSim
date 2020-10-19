181bc0625c8950f1e1c761f1a9ccb572fa451cf3
Nigel Barink <nigelbarink@hotmail.com>
2020-10-19 13:47:34 +0200

    Smart coding | Level 6 Taak 1

---
+++ Model/CompetitionData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Model
+{
+    class CompetitionData<T>
+    {
+        private List<T> _list = new List<T>();
+
+        public void AddData(T data)
+        {
+            _list.Add(data);
+        }
+    }
+}
